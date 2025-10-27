b6f15f848352515f8a57d6bf625bc0a8b7d2c01a
Tazwer Shakil <tazwershakilshakil@gmail.com>
2025-10-27 22:39:59 +0600

    create a simple server site and connect to mongodb successfully

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config()
+const port = process.env.PORT || 5000;
+const app = express()
+const { MongoClient } = require('mongodb');
+// middleware
+app.use(cors())
+app.use(express.json())
+// mongodb connection
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gr8kgxz.mongodb.net/?appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri);
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+   
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res)=>{
+    res.send('SoloSphere now available')
+})
+
+app.listen(port, ()=>{
+    console.log(`SoloSphere running on ${port}`);
+})
