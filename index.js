aa79cc0ca51c88f22fd64e46511b29695fcdd86c
Tazwer Shakil <tazwershakilshakil@gmail.com>
2025-10-28 19:21:43 +0600

    added post method for bids section

--- index.js
+++ index.js
@@ -3,12 +3,11 @@ const cors = require('cors');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 const app = express()
-const { MongoClient } = require('mongodb');
+const { MongoClient, ObjectId } = require('mongodb');
 // middleware
 app.use(cors())
 app.use(express.json())
 // mongodb connection
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gr8kgxz.mongodb.net/?appName=Cluster0`;
 
@@ -25,6 +24,20 @@ async function run() {
             const result = await cursor.toArray()
             res.send(result)
         })
+        //get single job data api 
+        app.get('/job/:id', async (req, res) => {
+            const id = req.params.id;
+            const query =  {_id: new ObjectId(id)}
+            const result = await jobsCollection.findOne(query)
+            res.send(result)
+        })
+        // save bid data api 
+        app.post('/bid', async (req, res)=>{
+            const bidData = req.body
+            const result = await bidsCollection.insertOne(bidData)
+            res.send(result)
+        })
+        // get bid data 
 
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
@@ -32,7 +45,6 @@ async function run() {
     }
 }
 run().catch(console.dir);
-
 
 app.get('/', (req, res) => {
     res.send('SoloSphere now available')
