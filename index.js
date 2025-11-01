37bd5ece642c64e07cb725bce0c0abf83774172d
Tazwer Shakil <tazwershakilshakil@gmail.com>
2025-11-01 22:57:24 +0600

    impliment search filter system and added vercel.json file

--- index.js
+++ index.js
@@ -121,8 +121,11 @@ async function run() {
             const page = parseInt(req.query.page) - 1
             const filter = req.query.filter
             const sort = req.query.sort
-            let query = {}
-            if (filter) query = { category: filter }
+            const search = req.query.search
+            let query = {
+                title: { $regex: search, $options: 'i' }
+            }
+            if (filter) query.category = filter
             let options = {}
             if (sort) options = { sort: { deadline: sort === 'asc' ? 1 : -1 } }
             const result = await jobsCollection
@@ -135,8 +138,11 @@ async function run() {
         // get all jobs count
         app.get('/jobs-count', async (req, res) => {
             const filter = req.query.filter
-            let query = {}
-            if (filter) query = { category: filter }
+            const search = req.query.search
+            let query = {
+                title: { $regex: search, $options: 'i' }
+            }
+            if (filter) query.category = filter
             const count = await jobsCollection.countDocuments(query)
             res.send({ count })
         })
